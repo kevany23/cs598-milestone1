36cc5b330baa52c15f50a81dd43e29f2ff979933
alex huynh <alexhuynh1999@gmail.com>
2022-02-20 17:09:24 -0600

    added commons-csv to test, modified static_analysis script, fixed up AST.jar to pass in args straight from command line

--- static_analysis.py
+++ static_analysis.py
@@ -1,7 +1,6 @@
 import subprocess
 import argparse
 
-path = "commons-csv/src/main/java/org/apache/commons/csv/Token.java"
 parser = argparse.ArgumentParser()
 parser.add_argument('--representation')
 parser.add_argument('--input')
@@ -12,5 +11,5 @@ path = args.input
 file = rep + ".jar"
 file_out = rep + ".png"
 
-subprocess.run(['java', '-jar', file], input=path, encoding='ascii')
+subprocess.call(['java', '-jar', file, path])
 subprocess.call(['dot', '-Tpng', 'AST.dot', '-o', file_out])
